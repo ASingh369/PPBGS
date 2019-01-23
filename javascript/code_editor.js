95c031574dd0abada1ab1c755f26690614ea6fcc
Jimmy Palelil <jimmypalelil@gmail.com>
2019-01-22 21:49:33 -0800

    got javascript code

--- javascript/code_editor.js
+++ javascript/code_editor.js
@@ -9,12 +9,25 @@ var js = CodeMirror.fromTextArea(document.getElementById("js"), {
   lineNumbers: true
 });
 
+window.onload = function () {
+    var result = document.getElementById("result").contentWindow;
+    result.console.stdlog = console.log.bind(console);
+    result.console.logs = [];
+    result.console.log = function(){
+        result.console.logs.push(Array.from(arguments));
+        result.console.stdlog.apply(console, arguments);
+    };
+
+    compile();
+};
+
 function compile() {
   var html_final = html.getValue();
   var js_final = js.getValue();
   var result = document.getElementById("result").contentWindow.document;
+  var resultWindow = document.getElementById("result").contentWindow;
   result.open();
   result.writeln(html_final + "<script>" + js_final + "</script>");
   result.close();
+  console.log(resultWindow.console.logs);
 }
-compile();
