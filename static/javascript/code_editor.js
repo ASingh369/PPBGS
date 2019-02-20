d0cfe1e63cd2cde5b69a2e5b4c8be6284acfa3cf
summerjacket <dxmcxd@gmail.com>
2019-02-19 18:53:33 -0800

    use html mode

--- static/javascript/code_editor.js
+++ static/javascript/code_editor.js
@@ -3,7 +3,13 @@
 Split(['#html-editor-parent', '#js-editor-parent']);
 
 const editors = {
-  html: CodeMirror.fromTextArea($('#html-editor')[0]),
+  html: CodeMirror.fromTextArea($('#html-editor')[0], {
+    lineNumbers: true,
+    theme: 'neo',
+    mode: 'htmlmixed',
+    gutters: ['CodeMirror-lint-markers'],
+    lint: true,
+  }),
   js: CodeMirror.fromTextArea($('#js-editor')[0], {
     lineNumbers: true,
     theme: 'neo',
