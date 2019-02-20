a68ac0b69608ef677daa3c42908cfabf74e559bd
summerjacket <dxmcxd@gmail.com>
2019-02-19 17:44:10 -0800

    use codemirror

--- javascript/code_editor.js
+++ javascript/code_editor.js
@@ -1,3 +1,16 @@
-/* global Split */
+/* global $, CodeMirror, Split */
 
 Split(['#html-editor-parent', '#js-editor-parent']);
+
+const editors = {
+  html: CodeMirror.fromTextArea($('#html-editor')[0]),
+  js: CodeMirror.fromTextArea($('#js-editor')[0], {
+    lineNumbers: true,
+    theme: 'neo',
+    mode: 'javascript',
+    gutters: ['CodeMirror-lint-markers'],
+    lint: {
+      esversion: 6,
+    },
+  }),
+};
