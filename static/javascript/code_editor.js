d2dd0890b63a50ae0303f9133933e0b3e49c1274
summerjacket <dxmcxd@gmail.com>
2019-02-23 23:19:00 -0800

    started tests feature

--- static/javascript/code_editor.js
+++ static/javascript/code_editor.js
@@ -1,4 +1,12 @@
 /* global $, CodeMirror, Split */
+/* eslint-disable no-console */
+
+let codeFailedTests = false;
+
+const fail = x => {
+  codeFailedTests = true;
+  console.error(x);
+}
 
 // references to page elements
 const DOMElements = {
@@ -34,16 +42,28 @@ const editors = {
   }),
 };
 
-// update the html render. bottom half of the page
-const runCode = () => {
+// write code to the iframe
+// precondition: fn :: (html code, js code) -> string
+const writeToFrame = fn => {
   const iframeDocument = DOMElements.iframe.contentWindow.document;
   const htmlCode = editors.html.getValue();
   const jsCode = editors.js.getValue();
+  const cbCode = fn(htmlCode, jsCode);
 
   iframeDocument.open();
-  iframeDocument.write(`${htmlCode}<script>${jsCode}</script>`);
+  iframeDocument.write(cbCode);
   iframeDocument.close();
+
+  return {
+    html: htmlCode,
+    js: jsCode,
+    cb: cbCode,
+  };
 };
+
+// update the html render. bottom half of the page
+const runCode = () =>
+  writeToFrame((html, js) => `${html}<script>${js}</script>`);
 
 $('#run-button').click(runCode);
 
@@ -54,6 +74,31 @@ $(document).keydown(e => {
   }
 });
 
+const testCode = test => {
+  codeFailedTests = false;
+
+  const code = writeToFrame((html, js) => `${html}<script>${js}</script>`);
+
+  test.has.forEach(piece => {
+    const re = new RegExp(piece.regex);
+
+    if (!re.test(code.js)) {
+      fail(`Missing: ${piece.readable}`);
+    }
+  });
+
+  test.hasNot.forEach(piece => {
+    const re = new RegExp(piece.regex);
+
+    if (re.test(code.js)) {
+      fail(`Cannot use: ${piece.readable}`);
+    }
+  });
+
+  if (!codeFailedTests) {
+    console.log('yay! all tests passed!');
+  }
+};
 
 // fetch data
 const dataLoc = '/static/mock_data/exercise0.json';
@@ -61,6 +106,7 @@ const dataLoc = '/static/mock_data/exercise0.json';
 fetch(dataLoc)
   .then(x => x.json())
   .then(data => {
+    $('#submit-button').click(() => testCode(data.test));
     $('#task-placeholder').html(data.task);
     editors.html.setValue(data.html);
     editors.js.setValue(data.js);
